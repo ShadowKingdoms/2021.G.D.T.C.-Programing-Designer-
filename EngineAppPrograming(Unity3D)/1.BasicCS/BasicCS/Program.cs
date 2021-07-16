b59158134f425c961fe5481a47a728eccdc78645
downkhg <downkhg@gmail.com>
2021-07-16 11:20:48 +0900

    크리티컬데미지와 확률과 if문

--- EngineAppPrograming(Unity3D)/1.BasicCS/BasicCS/Program.cs
+++ EngineAppPrograming(Unity3D)/1.BasicCS/BasicCS/Program.cs
@@ -11,8 +11,8 @@ namespace BasicCS
         {//구문: 중괄호. 함수나 클래스와 같이 공간을 구별하는 데 사용됨.
             //ValMain();//함수의 호출
             //BasicTestMain();
-            PlayerAttackMonsterMain();
-            //CriticalAttackMain();
+            //PlayerAttackMonsterMain();
+            CriticalAttackMain();
             Console.ReadLine();//종료방지
         }//구문끝
         //매개변수: 함수안에 값을 전달하는 변수
@@ -54,10 +54,36 @@ namespace BasicCS
             nMonsterHp = nMonsterHp - nPlayerDemage;
             Console.WriteLine("2.MonsterHP:" + nMonsterHp + ",PlayerDemage:" + nPlayerDemage);
         }
-
+        //크리티컬데미지? 확률적으로 발생한다. 곱셈? -> 일정확률로 데미지가 150%의 데미지로 공격한다.
+        //플레이어가 몬스터를 공격하면 (몬스터의 체력)이 (일정확률)로 만약 [크리티컬데미지가 발생하면 (플레이어의 데미지*1.5)만큼 하고], 아니라면 데미지만큼 감소한다..
+        //플레이어가 몬스터를 공격하면 몬스터의 체력이 감소하는데, 이때 (일정확률)로 (크리니컬데미지가 발생하면 데미지의 1.5배만큼 감소)하고, 아니라면 데미지만큼 감소한다.
+        //일정확률? 사건이 일어날 확률? 상자안에 공을 준비한 것중에서 1개를 뽑을 확률. -> 랜덤
         static void CriticalAttackMain()
         {
-            //
+            int nPlayerDemage = 10;
+            int nMonsterHp = 100;
+            Random cRadom = new Random();
+            int nRandom = cRadom.Next(0, 3);//프로그래밍에서는 항상0부터 시작한다.
+            Console.WriteLine("1.MonsterHP:" + nMonsterHp );
+            if (nRandom == 1)
+            {
+                nMonsterHp = nMonsterHp - (int)((float)nPlayerDemage * 1.5f);
+                Console.WriteLine("Critical!!!:" + nPlayerDemage*1.5f);
+            }
+            else 
+            {
+                nMonsterHp = nMonsterHp - nPlayerDemage;
+                Console.WriteLine("PlayerDemage:" + nPlayerDemage);
+            }
+            Console.WriteLine("2.MonsterHP:" + nMonsterHp);
+            Console.WriteLine("Random:" + nRandom);
+        }
+        //가고 싶은장소를 입력받아, 입력받은 장소의 이름이 (마을,상점,필드)중 하나를 입력하면 ~입니다 라고 출력하고, 아니라면 !?!?!?!를 출력하라.
+        //데이터: 입력장소를 저장할 변수 -> 문자열: string
+        //알고리즘: 마을이면 마을입니다. 상점이면 상점입니다. 필드이면 필드입니다.
+        static void StageMain()
+        {
+
         }
     }
 }
