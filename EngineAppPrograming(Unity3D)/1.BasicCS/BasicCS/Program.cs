fd397f190e8edc60dbb8d65b5635829bff4fd82e
downkhg <downkhg@gmail.com>
2021-07-16 10:23:13 +0900

    플레이어가 몬스터 공격하기

--- EngineAppPrograming(Unity3D)/1.BasicCS/BasicCS/Program.cs
+++ EngineAppPrograming(Unity3D)/1.BasicCS/BasicCS/Program.cs
@@ -11,7 +11,8 @@ namespace BasicCS
         {//구문: 중괄호. 함수나 클래스와 같이 공간을 구별하는 데 사용됨.
             //ValMain();//함수의 호출
             //BasicTestMain();
-            CriticalAttackMain();
+            PlayerAttackMonsterMain();
+            //CriticalAttackMain();
             Console.ReadLine();//종료방지
         }//구문끝
         //매개변수: 함수안에 값을 전달하는 변수
@@ -37,6 +38,21 @@ namespace BasicCS
 
             Console.WriteLine("Score:" + nScore);
             Console.WriteLine("Rat:" + fRat);
+        }
+        //(플레이어?)가 (몬스터?)를 공격한다.
+        //플레이어가 몬스터를 공격하면 (몬스터의 체력)이 (플레이어의 데미지)만큼 감소한다.
+        //(단, 플레이어의 데미지는 10이고 몬스터의 체력은 100이다)
+        //데이터: 몬스터의체력, 플레이어의 공격력 -> 변수: 정수형->int
+        //알고리즘: 몬스터체력 - 플레이어공격력 = 몬스터체력
+        //1.습관적으로 코드를 짜고 있다면 입력,출력을 생각없이 넣고있을수있다. 
+        //2.데이터의 출력은 연산전 모든변수의 값과 연산후의 모든변수의 값을 출력하면, 프로그램이 정상작동하는가 확인할수있다.
+        static void PlayerAttackMonsterMain()
+        {
+            int nPlayerDemage = 10;
+            int nMonsterHp = 100;
+            Console.WriteLine("1.MonsterHP:" + nMonsterHp +",PlayerDemage:"+nPlayerDemage);
+            nMonsterHp = nMonsterHp - nPlayerDemage;
+            Console.WriteLine("2.MonsterHP:" + nMonsterHp + ",PlayerDemage:" + nPlayerDemage);
         }
 
         static void CriticalAttackMain()
