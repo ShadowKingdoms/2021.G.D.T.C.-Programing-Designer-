ff8a46c5e5f76bce54447033a2ecbf3890186c24
downkhg <downkhg@gmail.com>
2021-07-19 16:06:25 +0900

    이동,점프,플랫폼,아이템

--- EngineAppPrograming(Unity3D)/3.Platformmer2D/Assets/Scripts/Dynamic.cs
+++ EngineAppPrograming(Unity3D)/3.Platformmer2D/Assets/Scripts/Dynamic.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Dynamic : MonoBehaviour
 {
     public float Speed;
+    public float JumpPower;
+    public bool isJump;
+
+    public int Score;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +24,40 @@ public class Dynamic : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
             transform.position += Vector3.left * Speed * Time.deltaTime;
         //위아래로 움직이도록 만들기
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.position += Vector3.up * Speed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (isJump == false)
+            {
+                Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+                rigidbody.AddForce(Vector3.up * JumpPower);
+                isJump = true;
+            }
+        }
         if (Input.GetKey(KeyCode.DownArrow))
             transform.position += Vector3.down * Speed * Time.deltaTime;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 100, 20), "Score:" + Score);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJump = false;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "cherry")
+        {
+            Score++;
+            Destroy(collision.gameObject);
+        }
     }
 }
