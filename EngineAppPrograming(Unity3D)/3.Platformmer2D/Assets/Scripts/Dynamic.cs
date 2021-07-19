95e1808855f38bab42e0a20f9cc23075a66bcf40
downkhg <downkhg@gmail.com>
2021-07-19 12:07:35 +0900

    이동

---
+++ EngineAppPrograming(Unity3D)/3.Platformmer2D/Assets/Scripts/Dynamic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamic : MonoBehaviour
+{
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.position += Vector3.right * Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position += Vector3.left * Speed * Time.deltaTime;
+        //위아래로 움직이도록 만들기
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.position += Vector3.up * Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position += Vector3.down * Speed * Time.deltaTime;
+    }
+}
