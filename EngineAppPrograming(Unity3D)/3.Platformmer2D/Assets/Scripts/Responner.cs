1171165d5eb00021b2c617f2d1dd81c2c2b32f4b
downkhg <downkhg@gmail.com>
2021-07-20 15:15:54 +0900

    부활하기

---
+++ EngineAppPrograming(Unity3D)/3.Platformmer2D/Assets/Scripts/Responner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Responner : MonoBehaviour
+{
+    public GameObject objPlayer;
+    public bool isRespon = false;
+    public string strPrefabName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        strPrefabName = objPlayer.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (objPlayer == null)// && isRespon == false)
+        {
+            if (isRespon == false)
+                StartCoroutine(ProcessTimmer());
+        }
+    }
+
+    IEnumerator ProcessTimmer()
+    {
+        Debug.Log("ProcessTimmer 1");
+        isRespon = true;
+        yield return new WaitForSeconds(1);
+        GameObject prefabPlayer = Resources.Load("Prefabs/"+strPrefabName) as GameObject;
+        objPlayer = Instantiate(prefabPlayer, this.transform.position, Quaternion.identity);
+        objPlayer.name = prefabPlayer.name;
+        isRespon = false;
+        Debug.Log("ProcessTimmer 2");
+    }
+}
