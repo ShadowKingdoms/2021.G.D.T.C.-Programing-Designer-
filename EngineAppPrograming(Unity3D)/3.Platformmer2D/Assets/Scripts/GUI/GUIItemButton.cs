6050b86ff11594009abc04f17ae651c830b18c00
downkhg <downkhg@gmail.com>
2021-08-10 14:22:19 +0900

    인벤토리 제작 중...

---
+++ EngineAppPrograming(Unity3D)/3.Platformmer2D/Assets/Scripts/GUI/GUIItemButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIItemButton : MonoBehaviour
+{
+    public Text textItemName;
+    public Image imgItemSprite;
+
+    public void Set(Item.ITEM_KIND item_kind)
+    {
+
+    }
+
+    public void OnClickEvent(Item.ITEM_KIND item_kind)
+    {
+        GameManager.GetInstance().EventItemUsePlayer(item_kind);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
